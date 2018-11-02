22260764557224c1ffa6502ec610665e7d58e480
Noel Austin <naustin00@gmail.com>
2018-11-01 21:54:48 -0400

    adding to repo

---
+++ Place-Files.ps1
@@ -0,0 +1,6 @@
+﻿foreach($file in Get-Content 'files.txt')
+{
+  $TargetFile = $(Join-Path $pwd $file)
+
+  Copy-Item -Path "$pwd\output" -Destination  $TargetFile
+}
---
+++ Pull-Files.ps1
@@ -0,0 +1,6 @@
+﻿foreach($file in Get-Content 'files.txt')
+{
+  $TargetFile = $(Join-Path $pwd $file)
+
+  Copy-Item -Path $TargetFile -Destination "$pwd\output"
+}

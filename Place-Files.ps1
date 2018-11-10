63bc7ee819f4a4ad5cd652d8c92602bb35a11bcb
Noel Austin <naustin00@gmail.com>
2018-11-09 22:20:31 -0500

    updated Push-Files, appears to be working

--- Place-Files.ps1
+++
@@ -1,6 +0,0 @@
-﻿foreach($file in Get-Content 'files.txt')
-{
-  $TargetFile = $(Join-Path $pwd $file)
-
-  Copy-Item -Path "$pwd\output" -Destination  $TargetFile
-}
---
+++ Push-Files.ps1
@@ -0,0 +1,14 @@
+﻿# Read a list of file paths and build the folder structure taking the files from the Output folder
+
+
+foreach($file in Get-Content 'files.txt')
+{
+  $FullPath = Join-Path $pwd $file
+  
+  $FileName = $file.split('\')[($file.split('\').count -1)]
+  $TargetFile = Join-Path $pwd (Join-Path "output" $FileName)
+  $DestFolder = Split-Path -Path $FullPath -Parent
+
+
+  Copy-Item -Path $TargetFile -Destination  $DestFolder
+}
